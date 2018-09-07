50be128c254cba7d3ec3fe063b813f3b5f47dfe2
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-06 19:28:08 -0600

    Update Programming Exercise 1-9.java

--- Chapter 1/Programming Exercise 1-9.java
+++ Chapter 1/Programming Exercise 1-9.java
@@ -1,6 +1,8 @@
 class ProEx {
   public static void main(String[] args) {
-    System.out.println("area = width * heigth")
-    System.out.println(
+    System.out.println("Area of a rectangle")
+    System.out.println(4.5 * 7.9)
+    System.out.println("Perimeter of a retangle")
+    System.out.println(2 * (4.5 + 7.9))
   }
 }
