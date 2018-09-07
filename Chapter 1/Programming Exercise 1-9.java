9a7bba468358452a22abc49bdeb4acd5daf08d71
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-06 19:42:35 -0600

    Update Programming Exercise 1-9.java

--- Chapter 1/Programming Exercise 1-9.java
+++ Chapter 1/Programming Exercise 1-9.java
@@ -1,5 +1,6 @@
 class ProEx {
   public static void main(String[] args) {
+    // The application program displays the area and perimeter of a rectangle!
     System.out.println("Area of a rectangle");
     System.out.println(4.5 * 7.9);
     System.out.println("Perimeter of a retangle");
