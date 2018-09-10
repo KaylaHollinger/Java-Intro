0a5a9f4c1c91f9be73e98537d84ad246db2d81f5
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-10 17:47:40 -0600

    Update ProgrammingExercise13.java

--- Chapter 1/ProgrammingExercise13.java
+++ Chapter 1/ProgrammingExercise13.java
@@ -1,7 +1,10 @@
-// This application program
+// This application program solves for x in the function 
 public class ProgrammingExercise13 {
 	public static void main(String[] args) {
-		System.out.println(a = 3.4, b = 50.2, c = 2.1, d = 0.55, e = 44.5, f = 5.9
-    		System.out.println(x = (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)))
+		System.out.println("a = 3.4, b = 50.2, c = 2.1, d = 0.55, e = 44.5, f = 5.9");
+		System.out.println("x = ");
+    		System.out.println(((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+		System.out.println("y = ");
+		System.out.println(((3.4 * 5.9) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)));
 	}
 }
