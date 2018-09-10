38412db8c279021c3031af0bd688a3793925e4d9
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-10 17:40:46 -0600

    Update ProgrammingExercise13.java

--- Chapter 1/ProgrammingExercise13.java
+++ Chapter 1/ProgrammingExercise13.java
@@ -2,8 +2,6 @@
 public class ProgrammingExercise13 {
 	public static void main(String[] args) {
 		System.out.println(a = 3.4, b = 50.2, c = 2.1, d = 0.55, e = 44.5, f = 5.9
-    System.out.println(3.4 * x + 50.2 * y = 44.5)
-    System.out.println(2.1 * x + 0.55 * y = 5.9)
-    System.out.println(
+    		System.out.println(x = (((44.5 * 0.55) - (50.2 * 5.9)) / ((3.4 * 0.55) - (50.2 * 2.1)))
 	}
 }
