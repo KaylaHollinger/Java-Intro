e637c104e2582cfe04c3a237319a4f5f0be60e42
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-10 19:23:43 -0600

    Update ProgrammingExercise1.java

--- Chapter 2/ProgrammingExercise1.java
+++ Chapter 2/ProgrammingExercise1.java
@@ -1,2 +1,18 @@
-// The application program displays
+import java.util.Scanner; // Scanner is in the java.util pakage
+
 class ProgrammingExercise1 {
+  Public static void main(String[] args) {
+    // Create a Scanner object
+    Scanner input = new Scanner(System.in);
+    
+    // Prompt the user to enter a number
+    System.out.print("Enter a degree in Celsius: ");
+    double degreecelcius = input.nextDouble();
+    
+    // Compute the change to farenheit
+    double degreefarenheit = ((9 / 5) * degreecelcius + 32);
+    
+    //Display results
+    System.out.println(degreecelcius + " " + degreefarenheit);
+  }
+}
