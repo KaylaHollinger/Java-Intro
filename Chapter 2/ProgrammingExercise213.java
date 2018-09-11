d90099689752c150ea17c3734fe3a4ca4a3c66fc
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-11 17:44:43 -0600

    Update ProgrammingExercise213.java

--- Chapter 2/ProgrammingExercise213.java
+++ Chapter 2/ProgrammingExercise213.java
@@ -1,3 +1,18 @@
+import java.util.Scanner; // Scanner is in the java.util package
+
 class ProgrammingExercise213 {
   public static void main(String[] args) {
+    // Create a Scanner object
+    Scanner input = new Scanner(System.in);
     
+    // Prompt user to enter a number
+    System.out.print("Enter a monthly savings amount: ");
+    double savings = input.nextDouble();
+    
+    // Compute savings each month
+    double onemonthsavings = (savings * (1 + 0.00417));
+    
+    // Display results
+    System.out.println("Savings after 6 months is " + onemonthsavings);
+  }
+}
