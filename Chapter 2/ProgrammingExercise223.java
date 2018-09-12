80fa61bb4707afecb8f269d808b44cce7c24734a
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-11 18:42:30 -0600

    Update ProgrammingExercise223.java

--- Chapter 2/ProgrammingExercise223.java
+++ Chapter 2/ProgrammingExercise223.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 class ProgrammingExercise223 {
-  public static void main(String[] args) {
-    // Create a Scanner object
+	public static void main(String[] args) {
+		// Create a Scanner object
 		Scanner input = new Scanner(System.in);
 		
 		// Prompt user to enter a number
 		System.out.print("Enter the driving distance: ");
 		double distance = input.nextDouble();
-    System.out.print("Enter miles per gallon: ");
-		double distance = input.nextDouble();
-    System.out.print("Enter price per gallon: ");
-		double distance = input.nextDouble();
+		System.out.print("Enter miles per gallon: ");
+		double milespg = input.nextDouble();
+		System.out.print("Enter price per gallon: ");
+		double pricepg = input.nextDouble();
     
-    // Display results
-		System.out.println("The cost of driving is " + distance);
-  }
+		// Display results
+		System.out.println("The cost of driving is $" + cost);
+	  	
+	}
 }
