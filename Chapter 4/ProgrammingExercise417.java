5144b4c1143f0ec8f8226749d8327fccc8b1ece5
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-09-20 19:07:49 -0600

    Create ProgrammingExercise417.java

---
+++ Chapter 4/ProgrammingExercise417.java
@@ -0,0 +1,56 @@
+// Kayla Hollinger 9/19/18
+import java.util.Scanner;
+
+class ProgrammingExercise417 {
+	public static void main(String[] args) {
+		//create scanner object
+		Scanner input = new Scanner(System.in); 
+	
+		//enter value of variables
+		System.out.print("Enter a year: ");
+		int year = input.nextInt();
+		System.out.print("Enter a month: ");
+		String month = input.next();
+		//System.out.println("Month is '" + month + "'");
+				
+		if (month.equals("Jan")) {
+			System.out.println(month + " " + year + " has " + 31 + " days");
+		}	
+		//if (month.equals("Feb"))     (year / 4 
+			
+			//System.out.println(month + " " + year + " has " + 29 + " days");
+		//else (month.equals("Feb"))
+			//System.out.println(month + " " + year + " has " + 29 + " days");
+		//}
+		if (month.equals("Mar")) {
+			System.out.println(month + " " + year + " has " + 31 + " days");
+		}
+		if (month.equals("Apr")) {
+			System.out.println(month + " " + year + " has " + 30 + " days");
+		}
+		if (month.equals("May")) {
+			System.out.println(month + " " + year + " has " + 31 + " days");
+		}
+		if (month.equals("Jun")) {
+			System.out.println(month + " " + year + " has " + 30 + " days");
+		}
+		if (month.equals("Jul")) {
+			System.out.println(month + " " + year + " has " + 31 + " days");
+		}
+		if (month.equals("Aug")) {
+			System.out.println(month + " " + year + " has " + 31 + " days");
+		}
+		if (month.equals("Sep")) {
+			System.out.println(month + " " + year + " has " + 30 + " days");
+		}
+		if (month.equals("Oct")) {
+			System.out.println(month + " " + year + " has " + 31 + " days");
+		}
+		if (month.equals("Nov")) {
+			System.out.println(month + " " + year + " has " + 30 + " days.");
+		}
+		if (month.equals("Dec")) {
+			System.out.println(month + " " + year + " has " + 31 + " days.");
+		}
+	}
+}
