730dcb3b3711d7be494c86c8296fddb52689c5ca
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-05 14:12:17 -0600

    Update ProgrammingExercise511.java

--- Chapter 5/ProgrammingExercise511.java
+++ Chapter 5/ProgrammingExercise511.java
@@ -1,10 +1,17 @@
-class ProgrammingExercise511 {
+//Kayla Hollinger 10/5/18
+
+class ProgramingExercise511 {
 	public static void main(String[] args) {
-		int x =
-    (x >= 100 && x <= 200)
-    (x / 5 ^ x / 6)
-    
-    System.out.println(
-    
+		//list variable
+		int i = 100;
+		//create loop
+		while (i < 201) {
+			//state increment
+			i++;
+			//state division conditions
+			if ((i % 5 == 0) ^ (i % 6 == 0))
+			//print results
+				System.out.println(i);	
+		}	
 	}
 }
