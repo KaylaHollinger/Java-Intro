48e8e022449d50e10609f85b8916fd5f3eab266d
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-09 13:10:48 -0600

    Update and rename ProgrammingExercise511.java to ProgramingExercise511.java

---
+++ Chapter 5/ProgramingExercise511.java
@@ -0,0 +1,30 @@
+//Kayla Hollinger 10/5/18
+
+class ProgramingExercise511 {
+	public static void main(String[] args) {
+		
+		//list variable
+		int i = 99;
+		
+		//create loop
+		while (i < 201) {
+			
+			//state increment
+			i++;
+			
+			//state division conditions
+			if ((i % 5 == 0) ^ (i % 6 == 0))
+			
+			//print results			
+			System.out.print(i + " ");
+			if (i == 130)
+				System.out.print("\n");
+				
+			if (i == 162)
+				System.out.print("\n");
+				
+			if (i == 195)
+				System.out.print("\n");
+		}
+	}
+}
