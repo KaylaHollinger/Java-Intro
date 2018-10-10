636206e85d417d88933c9f722ea9929fd89a3fe8
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-10 13:41:03 -0600

    Update ProgrammingExercise517.java

--- Chapter 5/ProgrammingExercise517.java
+++ Chapter 5/ProgrammingExercise517.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+class ProgrammingExercise517 {
+	public static void main(String[] args) {
+		//create scanner object
+		Scanner input = new Scanner(System.in);
+		
+		//enter the value of the variable
+		System.out.print("Enter the number of lines: ");
+		int nol = input.nextInt();
+		
+		for (int p = 1; p <= nol; p++)  {
+			System.out.println( );
+			
+			for (int s = nol - p; s >= 1; s--) {
+				System.out.print("  ");
+			}
+			
+			for (int l = p; l >= 2; l--) {
+				System.out.print(l + " ");
+			}
+			
+			for (int r = 1; r <= p; r++) {
+				System.out.print(r + " ");
+			}
+		}
+	}
+}
