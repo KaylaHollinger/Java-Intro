1f63ac95e455060ff5c9769ff87d7535fa1f621c
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-19 02:06:58 -0600

    Create ProgrammingExercise617.java

---
+++ Chapter 6/ProgrammingExercise617.java
@@ -0,0 +1,10 @@
+//Kayla Hollinger 10/11/18
+//This program finds has the user enter a word to get how many vowels and how many consonants in the word.
+import java.util.Scanner;
+class ProgrammingExercise549 {
+	public static void main(String[] args) {
+		//Create scanner object
+		Scanner input = new Scanner(System.in);
+		
+		//Enter the word
+		System.out.print("Enter a word: ");
