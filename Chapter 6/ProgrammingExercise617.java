baf9079213b2b3e5b3be89933036125b18232ca1
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-19 05:13:52 -0600

    Update ProgrammingExercise617.java

--- Chapter 6/ProgrammingExercise617.java
+++ Chapter 6/ProgrammingExercise617.java
@@ -7,4 +7,19 @@ class ProgrammingExercise549 {
 		Scanner input = new Scanner(System.in);
 		
 		//Enter the word
-		System.out.print("Enter a word: ");
+		System.out.print("Enter n: ");
+		int n = input.nextInt();
+		
+		printMatrix(n);
+	}
+	
+	//Method to display matrix
+	public static void printMatrix(int n) {
+		for (int rows = 0; rows < n; rows++) {
+			for (int c = 0; c < n; c ++) {
+				System.out.print((int)(Math.random() * 2));
+			}
+			System.out.println();
+		}
+	}
+}
