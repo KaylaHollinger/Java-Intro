0d3b879634fa49f04c1c02f71c7eab1c15731117
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-25 14:08:20 -0600

    Update ProgrammingExercise625.java

--- Chapter 6/ProgrammingExercise625.java
+++ Chapter 6/ProgrammingExercise625.java
@@ -1,16 +1,17 @@
-//Kayla Hollinger 10/11/18
-//This program finds has the user enter a word to get how many vowels and how many consonants in the word.
+//Kayla Hollinger 10/25/18
+//(Convert milliseconds to hours, minutes, and seconds) Write a method that converts milliseconds to hours, minutes, and seconds using the following header: public static String convertMillis(long millis). The method returns a string as hours:minutes:seconds. For example, convertMillis(5500) returns a string 0:0:5, convertMillis(100000) returns a string 0:1:40, and convertMillis(555550000) returns a string 154:19:10.
+//This program finds has the user enter an amount of milliseconds and reads it back in an hour:minute:second format.
 import java.util.Scanner;
-class ProgrammingExercise549 {
+class ProgrammingExercise625 {
 	public static void main(String[] args) {
 		//Create scanner object
 		Scanner input = new Scanner(System.in);
 		
-		//Enter the word
+		//Enter the number of milliseconds
 		System.out.print("Enter n: ");
 		long millis = input.nextLong();
 		
-		System.out.println("hours:minutes:seconds: " convertMillis(millis));			
+		System.out.println("hours:minutes:seconds: " + convertMillis(millis));			
 	}
 	
 	//Method to display matrix
@@ -25,4 +26,3 @@ class ProgrammingExercise549 {
 		return millis + current;
 		}
 	}
-}
