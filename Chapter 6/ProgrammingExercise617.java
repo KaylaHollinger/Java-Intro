e6f246380b01579cc554e0266be819c00f5c54c8
KaylaHollinger <43019086+KaylaHollinger@users.noreply.github.com>
2018-10-25 13:53:03 -0600

    Update ProgrammingExercise617.java

--- Chapter 6/ProgrammingExercise617.java
+++ Chapter 6/ProgrammingExercise617.java
@@ -1,7 +1,8 @@
-//Kayla Hollinger 10/11/18
-//This program finds has the user enter a word to get how many vowels and how many consonants in the word.
+//Kayla Hollinger 10/25/18
+//(Display matrix of 0s and 1s) Write a method that displays an n-by-n matrix using the following header:public static void printMatrix(int n). Each element is 0 or 1, which is generated randomly. Write a test program that prompts the user to enter n and displays an n-by-n matrix.
+//This program finds has the user enter a number and then randomly generates a random n by n matrix made of 1s and 0s.
 import java.util.Scanner;
-class ProgrammingExercise549 {
+class ProgrammingExercise617 {
 	public static void main(String[] args) {
 		//Create scanner object
 		Scanner input = new Scanner(System.in);
